774a72ce1c73d9057be185ecfd9ebfe7fe7f432c
xjy <201250071@smail.nju.edu.cn>
2023-01-10 23:13:14 +0800

    lab7_basic

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,103 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
+    private BSTNode root;
+
+    private class BSTNode {
+        private K key;
+        private V value;
+        private BSTNode left, right;
+        private int size;
+
+        public BSTNode(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
+    }
+
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return get(root, key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        BSTNode target = get(root, key);
+        return target == null ? null : target.value;
+    }
+
+    private BSTNode get(BSTNode node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(BSTNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private BSTNode put(BSTNode x, K key, V value) {
+        if (x == null) return new BSTNode(key, value, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
